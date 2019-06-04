90af2eda1859a56b3df56296d4b986a5b349e999
Lahari <lahari.ui@gmail.com>
2019-06-04 15:42:23 -0400

    WIP

--- src/decorators/warming.ts
+++ src/decorators/warming.ts
@@ -1,25 +1,26 @@
 import { LikeAPIGatewayProxyHandler, Warming } from '../types';
-import { nullLogger } from '../utils';
+// import { nullLogger } from '../utils';
+//
+// const defaultPayloadCheck = ({ source = '' }) => {
+//   return source === 'serverless-plugin-warmup';
+// };
 
-const defaultPayloadCheck = ({ source = '' }) => {
-  return source === 'serverless-plugin-warmup';
-};
-
-export const warming: Warming = (targetHandler, options = {}) => {
-  const { eventPayloadCheck = defaultPayloadCheck, logger = nullLogger } = options;
-
-  const wrappedHandler: LikeAPIGatewayProxyHandler = async (...args) => {
-    const { 0: event = {}, 1: context } = args;
-
-    if (eventPayloadCheck(event)) {
-      const response = { body: 'Accepted', statusCode: 202 };
-      logger.info('Warming event found. Exiting early.');
-      return response;
-    }
-
-    const response = await targetHandler(event, context);
-    return response;
-  };
-
-  return wrappedHandler;
+export const warming: Warming = (targetHandler) => {
+  return targetHandler as LikeAPIGatewayProxyHandler;
+  // const { eventPayloadCheck = defaultPayloadCheck, logger = nullLogger } = options;
+  //
+  // const wrappedHandler: LikeAPIGatewayProxyHandler = async (...args) => {
+  //   const { 0: event = {}, 1: context } = args;
+  //
+  //   if (eventPayloadCheck(event)) {
+  //     const response = { body: 'Accepted', statusCode: 202 };
+  //     logger.info('Warming event found. Exiting early.');
+  //     return response;
+  //   }
+  //
+  //   const response = await targetHandler(event, context);
+  //   return response;
+  // };
+  //
+  // return wrappedHandler;
 };
